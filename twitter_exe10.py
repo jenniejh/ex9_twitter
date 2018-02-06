62824713a7b36cd3c720a4f2afd1241e4953cb4e
Jennie <jingweihu.jh@gmail.com>
2018-02-06 15:21:50 -0500

    add_exe10

---
+++ twitter_exe10.py
@@ -0,0 +1,30 @@
+
+from requests_oauthlib import OAuth1Session
+import secrets
+import json
+
+client_key = secrets.client_key
+client_secret = secrets.client_secret
+resource_owner_key = secrets.access_token
+resource_owner_secret = secrets.access_token_secret
+
+protected_url = 'https://api.twitter.com/1.1/account/settings.json'
+
+oauth = OAuth1Session(client_key,
+                          client_secret=client_secret,
+                          resource_owner_key=resource_owner_key,
+                          resource_owner_secret=resource_owner_secret)
+
+protected_url = 'https://api.twitter.com/1.1/search/tweets.json'
+params = {'q':'food'}
+r = oauth.get(protected_url, params=params)
+data = json.loads(r.text)
+# print (r.text)
+
+for i in data["statuses"]:
+	print ("------------------")
+	print(i["user"]["name"])
+	print(i["text"])
+
+
+
